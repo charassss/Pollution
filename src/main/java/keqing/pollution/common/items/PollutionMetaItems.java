9380a6813bd2b90fb0ca3351cd5fa770af26b141
KeQing <1158677939@qq.com>
2024-04-07 15:05:33 +0800

    魔力系列电路板

--- src/main/java/keqing/pollution/common/items/PollutionMetaItems.java
+++ src/main/java/keqing/pollution/common/items/PollutionMetaItems.java
@@ -9,7 +9,21 @@ public class PollutionMetaItems {
     public static MetaItem<?>.MetaValueItem COLDCORE;
     public static MetaItem<?>.MetaValueItem INTEGRATECORE;
     public static MetaItem<?>.MetaValueItem SEGREGATECORE;
-
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_ULV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_LV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_MV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_HV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_EV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_IV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_LuV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_ZPM;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_UV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_UHV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_UEV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_UIV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_UXV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_OpV;
+    public static MetaItem<?>.MetaValueItem MAGIC_CIRCUIT_MAX;
 
     public static void initialization()
     {
