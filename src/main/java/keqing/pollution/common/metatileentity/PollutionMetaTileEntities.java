e85bb0e779bd51f86cbe2dc75834bddedab52510
catsatori <163221322+catsatori@users.noreply.github.com>
2024-04-21 04:07:08 +0800

    更新太多了，数不过来了喵

--- src/main/java/keqing/pollution/common/metatileentity/PollutionMetaTileEntities.java
+++ src/main/java/keqing/pollution/common/metatileentity/PollutionMetaTileEntities.java
@@ -49,6 +49,9 @@ public class PollutionMetaTileEntities {
     public static MetaTileEntityMagicBattery MAGIC_BATTERY;
     public static MetaTileEntityMagicDistillery MAGIC_DISTILLERY;
     public static MetaTileEntityMagicAlloyBlastSmelter MAGIC_ALLOY_BLAST;
+    public static MetaTileEntityMagicChemicalReactor MAGIC_CHEMICAL_REACTOR;
+    public static MetaTileEntityMagicAutoclave MAGIC_AUTOCLAVE;
+    public static MetaTileEntityMagicExtruder MAGIC_EXTRUDER;
     public static MetaTileEntityVisHatch[] VIS_HATCH = new MetaTileEntityVisHatch[14];
     public static MetaTileEntityTankHatch[] TANK_HATCH = new MetaTileEntityTankHatch[1];
 
@@ -89,6 +92,11 @@ public class PollutionMetaTileEntities {
         MAGIC_DISTILLERY = registerMetaTileEntity(15948, new MetaTileEntityMagicDistillery(gtqtcoreId("magic_distillery")));
         MAGIC_BATTERY = registerMetaTileEntity(15950, new MetaTileEntityMagicBattery(gtqtcoreId("magic_battery")));
         MAGIC_ALLOY_BLAST = registerMetaTileEntity(15951, new MetaTileEntityMagicAlloyBlastSmelter(gtqtcoreId("magic_alloy_blast")));
+        MAGIC_CHEMICAL_REACTOR = registerMetaTileEntity(15952, new MetaTileEntityMagicChemicalReactor(gtqtcoreId("magic_chemical_reactor")));
+        MAGIC_AUTOCLAVE = registerMetaTileEntity(15953, new MetaTileEntityMagicAutoclave(gtqtcoreId("magic_autoclave")));
+        MAGIC_EXTRUDER = registerMetaTileEntity(15954, new MetaTileEntityMagicExtruder(gtqtcoreId("magic_extruder")));
+
+
 
         for (int i = 1; i <= 8; i++) {
             String tierName = GTValues.VN[i].toLowerCase();
